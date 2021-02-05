f1b24c804cfc409eb0d0876feb0ccb2af57328c3
Rupak Roy <rupak2007bob1@yahoo.co.in>
2021-02-05 08:53:02 +0530

    model building coming soon

---
+++ plot_learning.py
@@ -0,0 +1,159 @@
+#semi auto ML app
+import streamlit as st
+
+#text to speech pckgs
+import pyttsx3
+
+#EDA pkgs
+import pandas as pd
+import numpy as np
+
+#data visualization pcks
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import seaborn as sns
+sns.set_theme(style="ticks", color_codes=True)
+
+#import plotly.graph_objects as go
+import plotly.express as px
+
+
+#ML pckgs
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
+
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+from sklearn.model_selection import GridSearchCV
+#disable warning message
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+
+
+def run_churn_plots():
+    
+    st.subheader(" Advance In-Depth Vizualizers Analysis")
+
+    data1 = st.file_uploader("Pre-Dataset Loaded",type = ["csv","txt"])
+    data = 'Churn_Modelling.csv'
+    if data is not None:
+        df = pd.read_csv(data,sep=',')
+        #st.dataframe(df.head(10))
+        selected_col = ['RowNumber','CustomerId','Surname']
+        df = df.drop(selected_col,axis=1)
+        df20=df.head(20)           
+        temp_numeric = df.select_dtypes(include=['int64','float64'])
+        t_n_c =temp_numeric.columns.to_list()
+        st.table(df20.style.bar(subset=t_n_c,align='mid', color=['#ffeceb', '#f0fff4']))
+        st.write("showing only 20 rows.....from", df.shape[0],"rows" )  
+        
+        #temp_char= df.select_dtypes(exclude=['int64','float64'])
+        #t_c_c =temp_char.columns.to_list()
+        #kind1 = ['line','scatter']
+        #col1 = ['Tenure','Gender','Geography','IsActiveMember','Exited']    
+    
+        if st.sidebar.checkbox("Character Variable Analysis"):
+            st.subheader("Specialized approach to visualization via Category.")
+            st.write("The process of understanding how variables in a dataset relate to each other and how those relationships depend on other variables.")
+            
+            plot_o1, plot_o2 = st.beta_columns((1,2))
+            
+            with plot_o1:    
+                Tenure1 = st.slider("Select Tenure great than & equal to",1,10,0,key='0svmk33')                
+                t = df.Tenure[df.Tenure>=Tenure1]
+                sns.catplot(x=t, y="Age", kind="swarm", data=df)
+                st.pyplot()
+
+               # sns.catplot(x="Age", kind="count", palette="ch:.25", data=df)
+                
+                sns.catplot(y="Exited", hue="Tenure", kind="count",palette="pastel", edgecolor=".6",data=df)
+                st.pyplot()
+                
+            with plot_o2:
+                sns.catplot(x="Tenure", y="Age", hue="Gender", kind="box",orient="v", data=df)
+                st.pyplot()
+                st.header("Category Variable Analysis")
+                st.subheader("\.........via Seaborn")
+            sns.catplot(x="NumOfProducts", y="CreditScore", hue="Gender",col="Geography", aspect=.7,kind="swarm", data=df)
+            st.pyplot()
+            
+            sns.catplot(x="Age", y="CreditScore", hue="Gender",col="Geography", aspect=.7,kind="swarm", data=df)
+            st.pyplot()
+        
+        if st.sidebar.checkbox("Numeric Variable Analysis"):
+            st.subheader("Specialized approach to visualization via Numbers.")
+            st.write("The process of understanding how variables in a dataset relate to each other and how those relationships depend on other variables.")
+                    
+            plot_o3, plot_o4 = st.beta_columns((.7,1.5))
+            with plot_o3:
+                sns.displot(df, x="Tenure", hue="Gender",multiple="stack")
+                st.pyplot()
+                
+                sns.displot(df, x="CreditScore", hue="Geography", kind="kde")
+                st.pyplot()
+                
+            with plot_o4:
+                sns.displot(df, x="CreditScore", hue="Geography", stat="density",element="step",multiple="stack")
+                st.pyplot()
+              
+            plot_o5, plot_o6 = st.beta_columns(2)
+            with plot_o5:
+                sns.displot(df, x="EstimatedSalary", y="Age", binwidth=(2, .5))
+                st.pyplot()
+                
+            with plot_o6:
+                #sns.displot(df, x="EstimatedSalary", y="CreditScore", binwidth=(2, .5), cbar=True)
+                sns.jointplot(data=df,x="Tenure", y="CreditScore", hue="Gender",kind="kde")
+                st.pyplot()
+    
+            sns.pairplot(df)
+            st.pyplot()
+        
+        if st.sidebar.checkbox("Multi-Variable Analysis"):
+            plot_o7, plot_o8,plot_o9 = st.beta_columns(3)
+            with plot_o7:
+                ordered_days = df.Tenure.value_counts().index
+                g = sns.FacetGrid(df, row="Tenure", row_order=ordered_days,
+                height=1.7, aspect=4,)
+                g.map(sns.kdeplot, "Age")
+                st.pyplot()
+            with plot_o8:
+                ordered_days = df.Tenure.value_counts().index
+                g = sns.FacetGrid(df, row="Tenure", row_order=ordered_days,
+                height=1.7, aspect=4,)
+                g.map(sns.kdeplot, "EstimatedSalary")
+                st.pyplot()
+            with plot_o9:
+                ordered_days = df.Tenure.value_counts().index
+                g = sns.FacetGrid(df, row="Tenure", row_order=ordered_days,
+                height=1.7, aspect=4,)
+                g.map(sns.kdeplot, "CreditScore")
+                st.pyplot()
+
+            #attend = sns.load_dataset("attention").query("subject <= 12")
+            g = sns.FacetGrid(df, col="Age", col_wrap=4, height=2, ylim=(0, 10))
+            g.map(sns.pointplot, "HasCrCard", "Tenure", color=".3", ci=None)
+            #need to fix order else incorrect plot g.map(sns.pointplot, "solutions", "score", order=[1, 2, 3], color=".3", ci=None)
+            st.pyplot()
+            
+            g = sns.PairGrid(df)
+            g.map_upper(sns.scatterplot)
+            g.map_lower(sns.kdeplot)
+            g.map_diag(sns.kdeplot, lw=3, legend=False)
+            st.pyplot()                
+            
+            
+            g = sns.pairplot(df, hue="Geography", palette="Set2", diag_kind="kde", height=2.5)
+            st.pyplot()
+    
---
+++ streamlit_chrun.py
@@ -0,0 +1,139 @@
+#semi auto ML app
+import streamlit as st
+
+#text to speech pckgs
+import pyttsx3
+
+#EDA pkgs
+import pandas as pd
+import numpy as np
+
+#data visualization pcks
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import seaborn as sns
+sns.set_theme(style="ticks", color_codes=True)
+#import plotly.graph_objects as go
+import plotly.express as px
+
+
+#ML pckgs
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
+
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+from sklearn.model_selection import GridSearchCV
+#disable warning message
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.set_option('deprecation.showPyplotGlobalUse', False)
+# Use the full page instead of a narrow central column
+st.set_page_config(layout="wide",page_title="Customer Retention App",
+                   initial_sidebar_state="expanded",page_icon="🧊")
+
+#loading the plot module
+from plot_learning import run_churn_plots
+
+import pickle
+
+import base64
+import time
+timestr = time.strftime("%Y%m%d-%H%M%S")
+# Fxn to Download Result
+def download_link(object_to_download, download_filename, download_link_text):
+    d=pickle.dump(lr_model, open(filename, 'wb'))
+    # some strings <-> bytes conversions necessary here
+    b64 = base64.b64encode(object_to_download.encode()).decode()
+    
+    return f'<a href="data:file/txt;base64,{b64}" download="{d}">"click click"</a>'
+
+def make_downloadable(data):
+    csvfile = data.to_csv(index=False)
+    b64 = base64.b64encode(csvfile.encode()).decode()
+    new_filename = "Analysis_report_{}_.csv".format(timestr)
+    st.markdown("🤘🏻  Download CSV file ⬇️  ")
+    href = f'<a href="data:file/csv;base64,{b64}" download="{new_filename}">Click here!</a>'
+    st.markdown(href, unsafe_allow_html=True)
+    
+def main():
+    """Auto-Machine Learning App with Streamlit """
+    
+    st.title("Advanced Ai model for Customer Retention Churn Analysis")
+    activities = ["EDA","Plots","Model Building","About"]
+    
+    choice = st.sidebar.selectbox("Select Activity",activities)
+
+    
+    if choice == 'EDA':
+        st.subheader("Exploratory Data Analysis")
+
+        data1 = st.file_uploader("Pre-Dataset Loaded",type = ["csv","txt"])
+        data = 'Churn_Modelling.csv'
+        if data is not None:
+            df = pd.read_csv(data,sep=',')
+            #st.dataframe(df.head(10))
+            selected_col = ['RowNumber','CustomerId','Surname']
+            df = df.drop(selected_col,axis=1)
+            df20=df.head(20)           
+            temp_numeric = df.select_dtypes(include=['int64','float64'])
+            t_n_c =temp_numeric.columns.to_list()
+            st.table(df20.style.bar(subset=t_n_c,align='mid', color=['#ffeceb', '#f0fff4']))
+            st.write("showing only 20 rows.....from", df.shape[0],"rows" )  
+            
+            st.subheader("Visualizing statistical relationships")
+            st.write("The process of understanding how variables in a dataset relate to each other and how those relationships depend on other variables.")
+
+            eda_o1, eda_o2 = st.beta_columns((1,3))
+            
+            temp_char= df.select_dtypes(exclude=['int64','float64'])
+            t_c_c =temp_char.columns.to_list()
+            kind1 = ['line','scatter']
+            col1 = ['Tenure','Gender','Geography','IsActiveMember','Exited']            
+            
+            with eda_o1:
+                x1= st.selectbox("Select Variable 1 (numeric)",t_n_c)
+                y1 = st.selectbox("Select Variable 2 (numeric)",t_n_c)
+                hue1 = st.selectbox("Select Variable 3 (categorical)",t_c_c)
+                kind2= st.selectbox("Select Kind of plot",kind1)
+                style1 = st.selectbox("Select variable 4 (categorical)",t_c_c)
+                col2 = st.selectbox("Select variable 5 (categorical)",col1)
+            
+            with eda_o2:
+                #distribution of data
+                sns.relplot(data=df, x=x1, y=y1, hue=hue1, kind=kind2,style=style1,col=col2,col_wrap=3,legend="auto")
+                st.pyplot()
+               
+            if st.sidebar.checkbox("Summary Statistics"):
+                st.subheader("Summary of the data")
+                df_describe = df.describe()
+                st.table(df_describe.style.bar(subset=t_n_c,align='mid', color=['#ffeceb', '#f0fff4']))
+                
+                        
+            with st.beta_expander("Download The report"):
+                href = f'<a href="data:file/txt" download="d">Click here!</a>'
+                st.markdown(href, unsafe_allow_html=True)
+                       
+    elif choice == 'Plots':
+        run_churn_plots()
+        
+    elif choice == 'Model Building':
+        st.write("comming soon")
+
+    elif choice == 'About':
+        st.subheader("About")
+        st.text("Thank you for your time")
+        
+if __name__ == '__main__':
+    main()
+    
+    
